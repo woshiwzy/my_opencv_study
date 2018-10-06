a39fc735ad0260547184a3e2b7517d0abfe1b7b6
wangzy <wzyfly@sina.com>
2018-10-06 13:57:55 +0800

    添加部分注释

--- OpencvStudy/skin_detct/SkinDetect.cpp
+++ OpencvStudy/skin_detct/SkinDetect.cpp
@@ -9,7 +9,7 @@ Mat mask;
 int resizeRect=240;//图像大小（采集图像，处理特征的时候归一化大小）
 int sacnWidnowSize=resizeRect;
 
-//从数据文件夹中生成训练数据
+//从数据文件夹中生成训练数据,这里的数据可以通过CreateMLdata.cpp采集后，人工区别后放到ml_data下对应的文件夹中
 map<string, vector<vector<float>>> getMLdata(){
     
     //    int MAX_PATH=120;
@@ -23,7 +23,7 @@ map<string, vector<vector<float>>> getMLdata(){
     vector<string> files=Tool::listFiles(rootdir);
     map<string, vector<vector<float>>> dataMap;
     
-    
+    //注意这里的HOG特征参数，扫描窗口越小，越准确，但是也越耗时
     HOGDescriptor *hog=new HOGDescriptor(Size(sacnWidnowSize,sacnWidnowSize),
                                          Size(sacnWidnowSize/2,sacnWidnowSize/2),
                                          Size(sacnWidnowSize/4,sacnWidnowSize/4),
@@ -47,7 +47,7 @@ map<string, vector<vector<float>>> getMLdata(){
                 //             cout<<imgAbsPath<<" "<<mat.rows<<","<<mat.cols;
                 Mat newMat(resizeRect,resizeRect,CV_32F);
                 
-                resize(mat, newMat, Size(resizeRect,resizeRect));//归一化，都变成240X240大小
+                resize(mat, newMat, Size(resizeRect,resizeRect));//归一化，都变成240X240大小后再采集特征
                 
                 vector<float> descripters;
                 hog->compute(newMat, descripters);
@@ -141,23 +141,23 @@ int main()
         
         cout<<it->first<<" "<<it->second.size()<<" features "<<endl;
         
-        dataMatRow+=it->second.size();
+        dataMatRow+=it->second.size();//统计出所有的数据行
         
         vector<vector<float>> dataItems=it->second;
         
         for(size_t i=0,isize=dataItems.size();i<isize;i++){
             
-            vector<float> dataItem=dataItems.at(i);
+            vector<float> dataItem=dataItems.at(i);//把每一个特征取出来，转换成Mat
             
             Mat dataItemMat(1,(int)dataItem.size(),CV_32F);
             
-            for(int j=0,jsize=(int)dataItem.size();j<jsize;j++){
+            for(int j=0,jsize=(int)dataItem.size();j<jsize;j++){//特征单个vector转换成 1行多列的Mat（没找到更好的办法，不过这个方法也没啥问题）
                 dataItemMat.at<float>(0, j)=dataItem.at(j);
             }
            
-            dataMat.push_back(dataItemMat);
+            dataMat.push_back(dataItemMat);//把所有的单个Mat 组装成个一个大的训练集Mat
             
-            labelMatVector.push_back(getLabelMatNumber(it->first));
+            labelMatVector.push_back(getLabelMatNumber(it->first));//所有的数据label
             
 
         }
@@ -165,36 +165,23 @@ int main()
     }
     
     Mat labelMat;
-    
-    for(int j=0,jsize=(int)labelMatVector.size();j<jsize;j++){
+    for(int j=0,jsize=(int)labelMatVector.size();j<jsize;j++){//label的vector转Mat
         int label=labelMatVector.at(j);
-//        cout<<"label:"<<label<<endl;
-//        labelMat.at<int>(j, 1)=label;
         labelMat.push_back(label);
     }
-    
-//    cout<<labelMat<<endl;
-    cout<<"data mat type:"<<dataMat.type()<<endl;
-    
-//    dataMat.convertTo(dataMat, CV_32F);
-//    knn->train(dataMat);
-    
-    
+
+    //设置训练数据
     Ptr<TrainData> tData= TrainData::create(dataMat, ROW_SAMPLE, labelMat);
     knn->train(tData);
     
     cout<<"总的机器学习数据行:"<<dataMatRow<<" 列："<<dataMatCol<<endl;
-//    cout<<labelMat<<endl;
-//    cout<<dataMat<<endl;
+
     
-    
-    //打开摄像头开始识别图片
+    //一切准备工作完成，打开摄像头开始识别图片
     
     VideoCapture cam(0);
     if(!cam.isOpened())
         return 0;
-    
-    
     
     
     double fps;
@@ -205,6 +192,7 @@ int main()
     
     Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
     
+    //Hog特征采集器，用来采集摄像头图片区域的特征，这里的参数必须和CraeteMLData中采集数据时一样，不然数据特征纬度对不上，无法识别
     HOGDescriptor *hog=new HOGDescriptor(Size(sacnWidnowSize,sacnWidnowSize),
                                          Size(sacnWidnowSize/2,sacnWidnowSize/2),
                                          Size(sacnWidnowSize/4,sacnWidnowSize/4),
@@ -216,18 +204,17 @@ int main()
     {
         
         t = (double)cv::getTickCount();//帧率开始
-        
         cam >> input_image;
         
-        flip(input_image, input_image, 1);
+        flip(input_image, input_image, 1);//图片反转，MAC OS上opecv采集的图片是f左右相反的，这里特殊处理一下
         
         if(input_image.empty())
             return 0;
         
         
-        
+        //新建一个空Mat 存放皮肤区域数据
         output_mask = Mat::zeros(input_image.size(), CV_8UC1);
-        
+        //采集皮肤区域
         Tool::getSkinOutput(input_image, output_mask);
         
         
@@ -238,18 +225,21 @@ int main()
         hierarchy.clear();
         filterContours.clear();
         
+        //开始寻找轮廓
         findContours(output_mask, contours, hierarchy, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);
         
         vector<vector<Point>> hull(contours.size());
-        // 去除伪轮廓
+        // 去除一些没有用的区域（比如太小，或者比例和手脸偏差太大的区域，这些区域大概率不是人体面部区域）
         for (size_t i = 0; i < contours.size(); i++)
         {
             
+            //找到这些区域后，用矩形框标记出来
             Rect r=boundingRect(contours[i]);
             
-            float whs=r.width*1.0f/r.height;
-            float hws=r.height*1.0/r.width;
+            float whs=r.width*1.0f/r.height;//宽高比
+            float hws=r.height*1.0/r.width;//高宽比，其实用一个比例就可以了
             
+            //计算向量面积，也是一种排出法，排出更多的非肤色区域
             long abs=fabs(contourArea(Mat(contours[i]))) ;
             
             int ow=output_mask.cols;
@@ -258,49 +248,48 @@ int main()
             
             if((r.width>100 && r.height>100)/*&&(whs>=0.6f && whs<=1.6f)*/&&(abs> 1000) && hws>0.9f ){
                 
+                //把复合面部区域特征的区域保存下来
                 filterContours.push_back(contours[i]);
                 
+                //并且把这些区域标记出来
                 Tool::drawRectangle(output_image, r);
                 Tool::drawRectangle(output_mask, r);
                 Tool::drawRectangle(input_image,r);
                 
-                int resizeRect=240;
                
+                //新建一个resizeRect大小的区域，用来保存符合肤色区域数据，并且要把这块区域全部resize 采样时候的大小
+                //就是resizeRect的大小，方便取得一样维度的特征，而且并不会影响识别结果（为什么不会，这里自己要想清楚）
                 Mat srcMat=output_mask(r);
                 Mat newMat(resizeRect,resizeRect,srcMat.type());
                 
                 resize(srcMat, newMat, Size(resizeRect,resizeRect));//归一化，都变成240X240大小
                 
-                vector<float> tempdescripters;
-                hog->compute(newMat, tempdescripters);
+                vector<float> tempdescripters;//采集的特征放到了这里
+                hog->compute(newMat, tempdescripters);//采集这块图片的特征
                 Mat inputHogMat(1,(int)dataMatCol,CV_32F);
-                
+                //用这个for循环把特征vector变成Mat
                 for(int j=0,jsize=(int)tempdescripters.size();j<jsize;j++){
                     inputHogMat.at<float>(0, j)=tempdescripters.at(j);
                 }
-//                cout<<"input type:"<<inputHogMat.type()<<endl;
-//                Mat result;
-//                cout<<inputHogMat<<endl;
-                float response=knn->predict(inputHogMat);
-                string result=getLabelFromIndex( response);
-//
+
+                //识别结果，因为是KNN所有无论如何总会得到一个结果，这个结果必然是LabelMat中的一个
+                float response=knn->predict(inputHogMat);//得到的结果必然是LablMat中的一个
+                string result=getLabelFromIndex( response);//对应成文字
                 
+                //在多种窗口中绘制出来
                 Tool::drawText(r.tl(), result, output_mask);
-                 Tool::drawText(r.tl(), result, input_image);
-                 Tool::drawText(r.tl(), result, output_image);
+                Tool::drawText(r.tl(), result, input_image);
+                Tool::drawText(r.tl(), result, output_image);
                 
                 cout<<"结果result:"<<" response: "<<result<<endl;
-                
                 
             }
             
         }
         
-        output_mask.setTo(0);
-        
-        drawContours(output_mask, filterContours, -1, Scalar(255,0,0), CV_FILLED); //8, hierarchy);
-        
-        input_image.copyTo(output_image, output_mask);
+        output_mask.setTo(0);//使用前重置一下
+        drawContours(output_mask, filterContours, -1, Scalar(255,0,0), CV_FILLED); //8, hierarchy);//把肤色区域绘制进去
+        input_image.copyTo(output_image, output_mask);//成功把肤色区域拷贝过去
         
         
         //====================
@@ -308,17 +297,22 @@ int main()
         fps = 1.0 / t;
         sprintf(fpst, "fps:%.2f", fps);      // 帧率保留两位小数
         //帧率结束
-        
+        //以下是绘制帧率，目前帧率比较少，这个demo自然还有很多优化空间
         Tool::drawText(Point(100,100), string(fpst), output_mask);
-        
-        
-        imshow("input image msk", output_mask);
-        imshow("input image", input_image);
-        imshow("output image", output_image);
+        Tool::drawText(Point(100,100), string(fpst), input_image);
+        Tool::drawText(Point(100,100), string(fpst), output_image);
+        //窗口太多会y影响帧率
+//        imshow("input image msk", output_mask);//Mask窗口
+        imshow("input image", input_image);//原始图
+//        imshow("output image", output_image);//输出图
         
         output_image.setTo(0);
-        if(27 == waitKey(30))
+        
+        if(27 == waitKey(30)){
+            delete hog;
             return 0;
+        }
+        
     }
     return 0;
 }
